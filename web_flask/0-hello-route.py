4aa55378543699c19558d716d06ed9802c9da5fc
riley-kyule <phillipkyule@gmail.com>
2023-05-12 13:22:19 +0300

    Task 0. Hello Flask!

---
+++ web_flask/0-hello-route.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Start Flask web app listening on port 0.0.0.0:5000
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_route():
+    """
+    Displays Hello HBNB
+    """
+    return "Hello HBNB!"
+
+
+if __name == "__main__":
+    app.run(host="0.0.0.0")
