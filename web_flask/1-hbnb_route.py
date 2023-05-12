1e9827aaaa8ae49274c08e9a95856b12f675fc17
riley-kyule <phillipkyule@gmail.com>
2023-05-12 13:51:11 +0300

    task 1. HBNB

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Starts a Flask Web App
+
+App listens on 0.0.0.0:5000
+Routes:
+    /: Displays 'Hello HBNB!'
+    /hbnb: Displays 'HBNB'
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Displays 'Hello HBNB!'"""
+    return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Displays 'HBNB'"""
+    return "HBNB"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
