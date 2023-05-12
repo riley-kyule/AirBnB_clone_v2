ddc68a2f578ad7003bc956a837da3ac055a37eb2
riley-kyule <phillipkyule@gmail.com>
2023-05-12 14:13:47 +0300

    Made file executable

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -10,14 +10,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/", strict_slashes=False)
 
+@app.route("/", strict_slashes=False)
 def hello_hbnb():
     """Displays 'Hello HBNB!'"""
     return "Hello HBNB!"
 
-@app.route("/hbnb", strict_slashes=False)
 
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """Displays 'HBNB'"""
     return "HBNB"
